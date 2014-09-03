4cc84be8702fc7d7f6b3cfbf015696d0fd84ef46
KJDoles <kevin.doles@spsmail.cuny.edu>
2014-09-03 18:45:34 -0400

    Changed to include which versions of R, RStudio, and PSQL are being used and the number of variables in the data set 'sales'.

---
+++ DMAA.R
@@ -0,0 +1,10 @@
+# Which version of R and RStudio are you using?
+version 
+# I am using vesion 3.1.1 of R and version 0.98.1028 of RStudio
+# What version of PostgreSQL are you using?
+psql --version
+# I am using PostgreSQL version 1.18.1. 
+install.packages("DMwR")
+require(DMwR)
+nrow(sales)
+# There are 401,146 rows of data in sales. 
